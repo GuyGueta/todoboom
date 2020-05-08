b7f7e71229925d53b2d61706f9bb71803a04eade
guy gueta <guy.gueta@mail.huji.ac.il>
2020-05-08 15:49:49 +0300

    main func ready todoboom ex1

--- app/src/main/java/com/guy_gueta/todoboom/MainActivity.kt
+++ app/src/main/java/com/guy_gueta/todoboom/MainActivity.kt
@@ -2,11 +2,26 @@ package com.guy_gueta.todoboom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myButton = findViewById<Button>(R.id.button1)
+        val myEditText = findViewById<EditText>(R.id.editText1)
+        val myTextView = findViewById<TextView>(R.id.textView1)
+
+        myButton.setOnClickListener {
+            val input = myEditText.text
+            myTextView.text = input
+        }
+
     }
 }
